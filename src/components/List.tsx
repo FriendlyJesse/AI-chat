4b95bbd267b661bec18acf42b9715f92074e8f9a
Jesse <jessexiyu@foxmail.com>
2024-06-14 17:57:57 +0800

    update

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -4,6 +4,8 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import { useMessage } from '../context/MessageProvider'
+import { yellow } from '@mui/material/colors';
+import chatIcon from '../icon.svg'
 
 export default function ChatList() {
   const { messages } = useMessage()
@@ -12,12 +14,16 @@ export default function ChatList() {
     <List sx={{ width: '100%', margin: '0 auto', bgcolor: 'background.paper' }}>
       {
         messages.map(({ role, content }, index) => (
-          <ListItem key={index} alignItems="flex-start">
+          <ListItem style={{ marginBottom: 10 }} key={index} alignItems="flex-start">
             <ListItemAvatar>
-              <Avatar alt={role} />
+              {
+                role === 'user' ?
+                <Avatar sx={{ bgcolor: yellow[600] }} alt={role} src='1.png' /> :
+                <Avatar alt={role} src={chatIcon} />
+              }
             </ListItemAvatar>
             <ListItemText
-              primary={role}
+              primary={role === 'user' ? 'You' : role}
               secondary={content as string}
             />
           </ListItem>
